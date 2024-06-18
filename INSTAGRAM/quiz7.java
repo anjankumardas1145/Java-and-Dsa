68f976b661de04acffe36553d840495472c01c19
anjankumardas5 <anjantechnology507@gmail.com>
2024-06-18 23:32:47 +0530

    added to my quiz section

---
+++ INSTAGRAM/quiz7.java
@@ -0,0 +1,9 @@
+package INSTAGRAM;
+
+public class quiz7
+{
+    public static void main(String[] args) {
+        int b=10;
+        System.out.println(b++ * ++b);
+    }
+}
---
+++ INSTAGRAM/quiz8.java
@@ -0,0 +1,11 @@
+package INSTAGRAM;
+
+public class quiz8 {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<3){
+            System.out.print(i +" ");
+            i++;
+        }
+    }
+}

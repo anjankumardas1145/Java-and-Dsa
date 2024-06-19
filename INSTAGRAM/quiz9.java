8a55e33b644bd2bece4705d3e1a99a9b661938d2
anjankumardas5 <anjantechnology507@gmail.com>
2024-06-19 23:44:47 +0530

    added to quiz

---
+++ INSTAGRAM/quiz9.java
@@ -0,0 +1,13 @@
+package INSTAGRAM;
+
+public class quiz9 {
+    public static void main(String[] args) {
+        int var1=9;
+        int var2=8;
+        if((var1=1)==var2){
+            System.out.println(var1);
+        }else{
+            System.out.println(++var1);
+        }
+    }
+}

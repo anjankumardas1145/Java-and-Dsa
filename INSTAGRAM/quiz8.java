d8e06ff3bff3d45ad2ae40fa4fccb34f569bc871
anjankumardas5 <anjantechnology507@gmail.com>
2024-06-23 23:28:15 +0530

    change

--- INSTAGRAM/quiz8.java
+++ INSTAGRAM/quiz8.java
@@ -3,7 +3,7 @@ package INSTAGRAM;
 public class quiz8 {
     public static void main(String[] args) {
         int i=0;
-        while (i<3){
+        while (i<30){
             System.out.print(i +" ");
             i++;
         }

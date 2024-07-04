06658acbfb4def14ee9cab8ff791e002a8dbee56
anjankumardas5 <anjantechnology507@gmail.com>
2024-07-04 23:07:13 +0530

    variable valu change

--- FUNCTION/sum_print.java
+++ FUNCTION/sum_print.java
@@ -8,9 +8,9 @@ public class sum_print {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a:");
         int a=sc.nextInt();
-        System.out.println("enter b:");
-        int b=sc.nextInt();
-        int sum=a+b;
+        System.out.println("enter c:");
+        int c=sc.nextInt();
+        int sum=a+c;
         System.out.println("the sum is: "+sum);
     }
     public static void main(String[] args) {

67b11d204c5c525c04bd848c895151bf7f2f7f2e
anjankumardas5 <anjantechnology507@gmail.com>
2024-08-01 23:56:52 +0530

    value change

--- Naveen_Reddy_Java/InputComplete_Calculator.java
+++ Naveen_Reddy_Java/InputComplete_Calculator.java
@@ -5,12 +5,12 @@ public class InputComplete_Calculator {
     public static void main(String[] args) {
         VeryAdv_Calc A=new VeryAdv_Calc();
         int R1=A.add();
-        int R2=A.sub();
+        int R7=A.sub();
         int R3=A.mul();
         int R4=A.div();
         int R5=A.mod();
         double R6= A.power();
         System.out.println(" Th power of the number is: " +R6);
-        System.out.println("The result is: " + "The sum is: "+R1 +" ,The sub is: "+R2 +" ,The mul is: "+R3 +" ,The div is: " +R4+ " ,The modulous is : "+R5);
+        System.out.println("The result is: " + "The sum is: "+R1 +" ,The sub is: "+R7 +" ,The mul is: "+R3 +" ,The div is: " +R4+ " ,The modulous is : "+R5);
     }
 }

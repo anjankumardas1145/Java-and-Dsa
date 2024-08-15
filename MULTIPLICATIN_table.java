1a0c469e6ab6b6dd34b31ea8e034a19643d86b16
anjankumardas5 <anjantechnology507@gmail.com>
2024-08-15 23:57:15 +0530

    vvv

--- MULTIPLICATIN_table.java
+++ MULTIPLICATIN_table.java
@@ -4,9 +4,9 @@ public class MULTIPLICATIN_table {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the number:");
-        int a=sc.nextInt();
+        int b=sc.nextInt();
         for (int i=1;i<=10;i++){
-            System.out.println(a+"X"+i+"="+a*i);
+            System.out.println(b+"X"+i+"="+b*i);
         }
     }
 }
---
+++ Naveen_Reddy_Java/WrapperbyAnuj.java
@@ -0,0 +1,13 @@
+package Naveen_Reddy_Java;
+import java.util.*;
+public class WrapperbyAnuj {
+    public static void main(String[] args) {
+       //Integer obj1=new Integer(7);//in the recent version of java integer constructor is depricitaed ,instead of this we use Integer.valueOf
+        String obj6=String.valueOf("abc");
+        Integer ob3=12;//autoboxing
+        System.out.println(obj6);
+
+        int and=ob3;
+        System.out.println(and);
+    }
+}
